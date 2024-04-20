ee3bf6af0b1824c873ac50918536c7a82c3b1edc
suba <84484832+suba327777@users.noreply.github.com>
2024-04-20 23:01:34 +0900

    add: api to send username

--- main.go
+++ main.go
@@ -1,11 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func getHello() string {
 	return "hello world"
 }
 
+func mountainHandler(w http.ResponseWriter, r *http.Request) {
+	username := r.URL.Query().Get("username")
+	if username == "" {
+		username = "Anonymous"
+	}
+
+	// レスポンスを返す
+	fmt.Fprintf(w, "Hello, %s! The server is healthy.", username)
+}
+
 func main() {
 	fmt.Println(getHello())
+	http.HandleFunc("/mountain", mountainHandler)
+
+	fmt.Println("server is runnning")
+	http.ListenAndServe(":8080", nil)
 }
