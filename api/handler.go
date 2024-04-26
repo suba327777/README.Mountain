3b2da0b078bb8c974f902233dd6b55a6711a5409
suba <84484832+suba327777@users.noreply.github.com>
2024-04-26 13:03:18 +0900

    add: display svg over https

--- api/handler.go
+++ api/handler.go
@@ -18,6 +18,19 @@ func MountainHandler(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"totalCommit": totalCommit})
+	print(totalCommit)
 
+	svg := generateSVG()
+
+	c.Data(http.StatusOK, "image/svg+xml", []byte(svg))
+	// c.JSON(http.StatusOK, gin.H{"totalCommit": totalCommit})
+
+}
+
+// wip
+func generateSVG() string {
+	return `<svg xmlns="http://www.w3.org/2000/svg" width="200" height="100">
+            <rect x="10" y="10" width="180" height="80" fill="lightblue" stroke="black" stroke-width="2"/>
+            <text x="100" y="50" text-anchor="middle" alignment-baseline="middle" fill="black">Hello, SVG!</text>
+        </svg>`
 }
