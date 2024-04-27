7af7d43c207b8c3d7f7509172c007de24fd0bdeb
suba <84484832+suba327777@users.noreply.github.com>
2024-04-27 17:55:17 +0900

    add: create general framework card

--- ui/card.go
+++ ui/card.go
@@ -1,8 +1,36 @@
 package ui
 
-func GenerateCard() string {
-	return `<svg xmlns="http://www.w3.org/2000/svg" width="200" height="100">
-            <rect x="10" y="10" width="180" height="80" fill="lightblue" stroke="black" stroke-width="2"/>
-            <text x="100" y="50" text-anchor="middle" alignment-baseline="middle" fill="black">Hello, SVG!</text>
-        </svg>`
+import (
+	"fmt"
+)
+
+func startSVG() string {
+	return `<svg xmlns="http://www.w3.org/2000/svg">`
+}
+
+func rect(width, height int, bgColor string) string {
+	return fmt.Sprintf(`<rect x="10" y="10" width="%d" height="%d" fill="%s" rx="5" ry="5" stroke="black" stroke-width="2"/>`, width, height, bgColor)
+}
+
+func title(username string) string {
+	return fmt.Sprintf(`<text x="110" y="30" font-size="15" dominant-baseline="middle" text-anchor="middle" fill="white">%s's mountain</text>`, username)
+}
+
+func endSVG() string {
+	return `</svg>`
+}
+func GenerateCard(username string) string {
+
+	width := 340
+	height := 200
+	bgColor := "#141321"
+
+	svg := startSVG()
+	svg += rect(width, height, bgColor)
+	svg += title(username)
+
+	svg += endSVG()
+	fmt.Println(svg)
+
+	return svg
 }
