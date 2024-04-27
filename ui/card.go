bcb5c49265f728a5736e8233a807f3d81c5607f3
suba <84484832+suba327777@users.noreply.github.com>
2024-04-28 01:49:09 +0900

    add: left info svg

--- ui/card.go
+++ ui/card.go
@@ -5,7 +5,7 @@ import (
 )
 
 func startSVG(width, height int, viewBox string) string {
-	return fmt.Sprintf(`<svg xmlns="http://www.w3.org/2000/svg" width="%d" height="%d" viewBox="%s">`, width, height, viewBox)
+	return fmt.Sprintf(`<svg xmlns="http://www.w3.org/2000/svg" width="%d" height="%d" viewBox="%s" >`, width, height, viewBox)
 }
 
 func rect(width, height int, bgColor string) string {
@@ -15,10 +15,29 @@ func rect(width, height int, bgColor string) string {
 func title(username string, mountain string) string {
 	return fmt.Sprintf(`
     <g>
-      <g transform="translate(5,5)">%s</g>
-        <text x="40" y="23" font-size="14" dominant-baseline="middle" text-anchor="start" fill="white">%s</text>
+      <g transform="translate(5,8)">%s</g>
+        <text x="40" y="25" font-size="14" dominant-baseline="middle" text-anchor="start" fill="white">%s</text>
     </g>
     `, mountain, username)
+}
+
+func leftInfo(width int, tree, climber, Backpack string) string {
+	return fmt.Sprintf(`
+  <g>
+    <g>
+      <g transform="translate(5,65)">%s</g>
+      <text x="35" y="75" font-size="11" dominant-baseline="middle" text-anchor="start" fill="white">test: %d</text>
+    </g>
+    <g>
+      <g transform="translate(5,110)">%s</g>
+      <text x="35" y="120" font-size="11" dominant-baseline="middle" text-anchor="start" fill="white">test: %d</text>
+    </g>
+    <g>
+      <g transform="translate(5,155)">%s</g>
+      <text x="35" y="165" font-size="11" dominant-baseline="middle" text-anchor="start" fill="white">test: %d</text>
+    </g>
+  </g>
+  `, tree, width, climber, width, Backpack, width)
 }
 
 func endSVG() string {
@@ -34,6 +53,7 @@ func GenerateCard(username string) string {
 	svg := startSVG(width, height, viewBox)
 	svg += rect(width, height, bgColor)
 	svg += title(username, Mountain)
+	svg += leftInfo(width, Tree, Climber, Backpack)
 
 	svg += endSVG()
 
