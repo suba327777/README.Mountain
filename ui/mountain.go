b81745ff8a6246cec87252a451758b0167109015
suba <84484832+suba327777@users.noreply.github.com>
2024-04-28 08:44:42 +0900

    refactor: move icon definition

--- ui/mountain.go
+++ ui/mountain.go
@@ -22,12 +22,6 @@ func mountainColor(todayCommit int) string {
 	return fmt.Sprintf(`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000" height="20" width="20"><path fill="%s" d="M860,880H80c0-173.49,24.31-307,75.31-427.75,43.1-102,121.1-216.84,196-292.25h44.28c-19.43,64.11-53,213.82-59.95,295.3,38.81-100.94,112-209.91,195.67-295.3h44.28c-21.52,73.42-53.59,213.49-60,295.3,35-102.95,119.9-215.9,195.67-295.3h44.28c-23.28,82.4-53.93,213.87-60,295.3C734.48,354.16,814.13,236.08,891.34,160h44.28C875.88,390.82,860,523.39,860,880ZM120.14,840H820c.42-262.13,11.61-406.94,57.64-607.64-99.49,117.66-163,244-194,388.11H644.12c6.11-136.68,22.56-252.89,53.55-388.12-99.5,117.67-163,244-194,388.12H464.12c6.11-136.68,22.56-252.89,53.55-388.12-99.5,117.67-163,244-194,388.12H284.12c6.11-136.68,22.56-252.89,53.55-388.12C275.38,306,226.21,386.82,192.19,467.75,145.81,578.06,122.18,700,120.14,840Z"></path></svg>`, color)
 }
 
-func generateTrianglePath(x, y int) string {
-	return fmt.Sprintf(`<svg  xmlns="http://www.w3.org/2000/svg" width="25" height="25" x="%d" y="%d" viewBox="0 0 264.58 264.58">
-	  <path transform="matrix(.26458 0 0 .26458 -.001665 0)" d="m500 133c-10 0-20 5-25 15l-375 652c-30 50-25 75 25 75l375-0.26172 375 0.26172c50 0 55-25 25-75l-375-652c-5-10-15-15-25-15z" fill="#ADFF2F" stroke-width="3.7796"/>
-  </svg>`, x, y)
-}
-
 func generateMountain(count int) string {
 	var results strings.Builder
 
@@ -60,6 +54,18 @@ func generateMountain(count int) string {
 			x -= 30
 			y -= 15
 			results.WriteString(generateTrianglePath(x, y))
+		} else if i == 29 {
+			x -= 50
+			y -= 5
+			results.WriteString(generateStarPath(x, y))
+		} else if i == 30 {
+			x -= 30
+			y += 20
+			results.WriteString(generateStarPath(x, y))
+		} else if i == 31 {
+			x += 30
+			y -= 30
+			results.WriteString(generateFlagPath(x, y))
 		} else {
 			results.WriteString(generateTrianglePath(x, y))
 		}
