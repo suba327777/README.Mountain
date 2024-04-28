b5c9e86b116d65c55356f0b86d8620ba29cbf1f2
suba <84484832+suba327777@users.noreply.github.com>
2024-04-28 11:41:37 +0900

    add: fetch this month commit and this year commit

--- api/handler.go
+++ api/handler.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"errors"
+	"fmt"
 	"os"
 	"time"
 
@@ -22,7 +23,27 @@ func MountainHandler() ([]byte, error) {
 		return nil, err
 	}
 
-	dailyCommitsSince1MonthCount := commitCountDailySince1Month(user.DailyCommitsSince1Month)
+	dailyCommitsSince1MonthCount := dailyCommitsPeriodCount(user.DailyCommitsPeriod)
+	fmt.Println(dailyCommitsSince1MonthCount)
+
+	fromDate = time.Date(time.Now().Year(), time.Now().Month(), 1, 0, 0, 0, 0, time.UTC)
+	toDate = time.Date(time.Now().Year(), time.Now().Month()+1, 1, 0, 0, 0, -1, time.UTC)
+	user, err = getUserData(username, fromDate, toDate)
+	if err != nil {
+		return nil, err
+	}
+	dailyCommitsMonthCount := dailyCommitsPeriodCount(user.DailyCommitsPeriod)
+	fmt.Println(dailyCommitsMonthCount)
+
+	fromDate = time.Date(time.Now().Year(), 1, 1, 0, 0, 0, 0, time.UTC)
+	toDate = time.Date(time.Now().Year(), 12, 31, 23, 59, 59, 999999999, time.UTC)
+	user, err = getUserData(username, fromDate, toDate)
+	if err != nil {
+		return nil, err
+	}
+
+	comiitsYearCount := commitsPeriodCount(user.DailyCommitsPeriod)
+	fmt.Println(comiitsYearCount)
 
 	svg := ui.GenerateCard(username, dailyCommitsSince1MonthCount)
 
