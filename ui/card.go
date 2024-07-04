45c6e81f754058df68bf1d4947ac2abcb03ac982
suba <84484832+suba327777@users.noreply.github.com>
2024-07-04 23:10:53 +0900

    fix: lint error

--- ui/card.go
+++ ui/card.go
@@ -6,10 +6,10 @@ import (
 )
 
 func startSVG() string {
-	return fmt.Sprintf(`
+	return `
   <svg xmlns="http://www.w3.org/2000/svg" width="340" height="200" viewBox="0 0 340 200" >
     <a xlink:href="https://github.com/suba327777/README.Mountain">
-  `)
+  `
 }
 
 func rect(bgColor, borderColor string) string {
